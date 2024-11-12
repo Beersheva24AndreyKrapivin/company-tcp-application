061c2bd3bb6001a52f74ef95370614d7717c861a
Андрей Крапивин <without.a.body@gmail.com>
2024-11-13 00:01:21 +0200

    HW 40. Reflection

--- company-server-appl/src/main/java/telran/employees/CompanyProtocol.java
+++ company-server-appl/src/main/java/telran/employees/CompanyProtocol.java
@@ -1,5 +1,6 @@
 package telran.employees;
 
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import org.json.JSONArray;
@@ -20,27 +21,21 @@ public class CompanyProtocol implements Protocol {
         String data = request.requestData();
         Response response = null;
         try {
-            response = switch (type) {
-                case "addEmployee" -> addEmployeeServer(data);
-                case "getDepartmentBudget" -> getDepartmentBudgetServer(data);
-                case "getDepartments" -> getDepartmentsServer();
-                case "getEmployee" -> getEmployeeServer(data);
-                case "getManagersWithMostFactor" -> getManagersWithMostFactorServer();
-                case "removeEmployee" -> removeEmployeeServer(data);
-                default -> new Response(ResponseCode.WRONG_TYPE, type + " is wrong type");
-            };
+            Method method = CompanyProtocol.class.getDeclaredMethod(type, String.class);
+            method.setAccessible(true);
+            return (Response) method.invoke(this, data);
         } catch (Exception e) {
             response = new Response(ResponseCode.WRONG_DATA, e.getMessage());
         }
         return response;
     }
 
-    private Response removeEmployeeServer(String data) {
+    private Response removeEmployee(String data) {
         Employee employee = company.removeEmployee(Long.parseLong(data));
         return new Response(ResponseCode.OK, employee.toString());
     }
 
-    private Response getManagersWithMostFactorServer() {
+    private Response getManagersWithMostFactor(String data) {
         Manager[] managers = company.getManagersWithMostFactor();
         String[] strings = Arrays.stream(managers).map(Manager::toString).toArray(String[]::new);
         JSONArray jsonArray = new JSONArray(strings);
@@ -48,23 +43,23 @@ public class CompanyProtocol implements Protocol {
         return new Response(ResponseCode.OK, jsonArray.toString());
     }
 
-    private Response getEmployeeServer(String data) {
+    private Response getEmployee(String data) {
         Employee empl = company.getEmployee(Long.parseLong(data));
         return new Response(ResponseCode.OK, empl.toString());
     }
 
-    private Response getDepartmentsServer() {
+    private Response getDepartments(String data) {
         String[] departments = company.getDepartments();
         JSONArray jsonArray = new JSONArray(departments);
         return new Response(ResponseCode.OK, jsonArray.toString());
     }
 
-    private Response getDepartmentBudgetServer(String data) {
+    private Response getDepartmentBudget(String data) {
         int budget = company.getDepartmentBudget(data);
         return new Response(ResponseCode.OK, String.valueOf(budget));
     }
 
-    private Response addEmployeeServer(String data) {
+    private Response addEmployee(String data) {
         Employee employee = Employee.getEmployeeFromJSON(data);
         company.addEmployee(employee);
         return new Response(ResponseCode.OK, "");
