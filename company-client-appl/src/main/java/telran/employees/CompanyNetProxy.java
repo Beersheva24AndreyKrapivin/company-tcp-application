42a86b4d1847f2782c7466568b1597e060bec0c5
Андрей Крапивин <without.a.body@gmail.com>
2024-11-14 15:07:10 +0200

    Add net

--- company-client-appl/src/main/java/telran/employees/CompanyNetProxy.java
+++ company-client-appl/src/main/java/telran/employees/CompanyNetProxy.java
@@ -1,17 +1,19 @@
 package telran.employees;
 
+import java.nio.channels.NetworkChannel;
 import java.util.Iterator;
 import java.util.stream.IntStream;
 
 import org.json.JSONArray;
 
+import telran.net.NetworkClient;
 import telran.net.TcpClient;
 
-public class CompanyTcpProxy implements Company{
-    TcpClient tcpClient;
+public class CompanyNetProxy implements Company{
+    NetworkClient netClient;
 
-    public CompanyTcpProxy(TcpClient tcpClient) {
-        this.tcpClient = tcpClient;
+    public CompanyNetProxy(NetworkClient netClient) {
+        this.netClient = netClient;
     }
 
     @Override
@@ -22,32 +24,32 @@ public class CompanyTcpProxy implements Company{
 
     @Override
     public void addEmployee(Employee empl) {
-        tcpClient.sendAndReceive("addEmployee", empl.toString());
+        netClient.sendAndReceive("addEmployee", empl.toString());
     }
 
     @Override
     public int getDepartmentBudget(String department) {
-        String budgetString = tcpClient.sendAndReceive("getDepartmentBudget", department);
+        String budgetString = netClient.sendAndReceive("getDepartmentBudget", department);
         return Integer.parseInt(budgetString);
     }
 
     @Override
     public String[] getDepartments() {
-        String jsonString = tcpClient.sendAndReceive("getDepartments", "");
+        String jsonString = netClient.sendAndReceive("getDepartments", "");
         JSONArray jsonArray = new JSONArray(jsonString);
         return jsonArray.toList().toArray(String[]::new);
     }
 
     @Override
     public Employee getEmployee(long id) {
-        String jsonString = tcpClient.sendAndReceive("getEmployee", String.valueOf(id));   
+        String jsonString = netClient.sendAndReceive("getEmployee", String.valueOf(id));   
         return Employee.getEmployeeFromJSON(jsonString);
 
     }
 
     @Override
     public Manager[] getManagersWithMostFactor() {
-        String jsonString = tcpClient.sendAndReceive("getManagersWithMostFactor", "");
+        String jsonString = netClient.sendAndReceive("getManagersWithMostFactor", "");
         JSONArray jsonArray = new JSONArray(jsonString);
 
         return jsonArray.toList().stream()
@@ -57,7 +59,7 @@ public class CompanyTcpProxy implements Company{
 
     @Override
     public Employee removeEmployee(long id) {
-        String jsonString = tcpClient.sendAndReceive("removeEmployee", String.valueOf(id));   
+        String jsonString = netClient.sendAndReceive("removeEmployee", String.valueOf(id));   
         return Employee.getEmployeeFromJSON(jsonString);    
     }
 
